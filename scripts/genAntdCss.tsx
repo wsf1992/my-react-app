7e4591dcaea6fd031f4fc7fd767a6bfec36f7bd2
wsf1992 <741987899@qq.com>
2023-07-06 23:29:33 +0800

    fix:解决 antd 自定义组件样式后，刷新页面 样式改变延迟的问题

--- scripts/genAntdCss.tsx
+++ scripts/genAntdCss.tsx
@@ -1,9 +1,34 @@
-import { extractStyle } from '@ant-design/static-style-extract';
+import { extractStyle } from '@ant-design/static-style-extract'
+import { ConfigProvider } from 'antd'
 
-import fs from 'fs';
+import React from 'react'
+import fs from 'fs'
 
-const outputPath = './public/antd.min.css';
+const outputPath = './public/antd.min.css'
 
-const css = extractStyle();
+const css = extractStyle((node: any) => (
+    <>
+        <ConfigProvider theme={{ token: {} }}>{node}</ConfigProvider>
+        <ConfigProvider
+            theme={{
+                components: {
+                    Button: {
+                        colorTextLightSolid: '#fff',
+                        colorTextDisabled: '#fff',
+                        colorPrimary: '#229dff',
+                        colorPrimaryHover: '#66b1ff',
+                        colorPrimaryActive: '#66b1ff',
+                        colorBgContainerDisabled: '#a0cfff',
+                        borderRadius: 50,
+                        fontSize: 20,
+                        controlHeight: 50
+                    }
+                }
+            }}
+        >
+            {node}
+        </ConfigProvider>
+    </>
+))
 
-fs.writeFileSync(outputPath, css);
+fs.writeFileSync(outputPath, css)
